a657201efcaf26f4a8c5ad04941431acc68a194e
teodoromatheus <matheusteodoro.r@gmail.com>
2023-12-22 22:28:54 -0300

    CI: Adicionando o random do arquivo csv como valor da API

--- back-end/API/API.py
+++ back-end/API/API.py
@@ -1,17 +1,25 @@
 from fastapi import FastAPI
 from faker import Faker
+import pandas as pd
+import random
 
 app = FastAPI()
 fake = Faker()
+df_products = pd.read_csv("back-end/API/products.csv",delimiter=";")
+df_products['indice'] = range(1,len(df_products)+1)
+df_products.set_index('indice',inplace=True)
 
 @app.get("/gerar_compra")
 async def gerar_compra():
+    index = random.randint(1,len(df_products)+1)
+    registro = df_products.iloc[index]
     return {
         "client": fake.name(),
         "creditCard": fake.credit_card_provider(),
-        "ean": "Código de Barra",
-        "price": "Preço do produto",
+        "ean": int(registro["ean"]),
+        "productName": registro["product"],
+        "price": round(float(registro["price"])*1.2,2),
         "store": 11,
         "dateTime": fake.iso8601(),
         "clientPosition": fake.location_on_land()
-    }
+    }   
