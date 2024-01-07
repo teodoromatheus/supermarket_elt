431b0990879685a13d042ea306d3814e2409d029
teodoromatheus <matheusteodoro.r@gmail.com>
2024-01-07 18:16:07 -0300

    CI: Elaboração do validador de Excel

--- frontend/data_source/excel_collector.py
+++ frontend/data_source/excel_collector.py
@@ -1,15 +1,27 @@
+import pandas as pd
+import openpyxl
+from datetime import datetime
+
 class ExcelCollector():
-    def __init__(self):
-        pass
+    def __init__(self, schema):
+        self._schema = schema
+        self._aws = None
+        self._excel = None
 
-    def start(self):
-        pass
+    def start(self, arquivo_excel):
+        data_frame = self.transformToDataFrame(arquivo=arquivo_excel)
+        errors = self.validateDataFrame(dataframe=data_frame)
+        return errors
+        
+    def transformToDataFrame(self, arquivo):
+        data_frame = pd.read_excel(arquivo, parse_dates=['Data'])
+        return data_frame
 
-    def extractExcel(self):
-        pass
-
-    def transformToDataFrame(self):
-        pass
-
-    def validateDataFrame(self):
-        pass
+    def validateDataFrame(self, dataframe):
+        errors = []
+        for index, row in dataframe.iterrows():
+            try:
+                self._schema(**row.to_dict())
+            except Exception as e:
+                errors.append(f'Erro linha {index+2}: {e}')
+        return errors
