07a6026280c444c302cf328b76ea906001f05a8a
teodoromatheus <matheusteodoro.r@gmail.com>
2024-01-06 23:36:23 -0300

    CI: Esboço e estruturação coletor de Excel

---
+++ frontend/data_source/excel_collector.py
@@ -0,0 +1,15 @@
+class ExcelCollector():
+    def __init__(self):
+        pass
+
+    def start(self):
+        pass
+
+    def extractExcel(self):
+        pass
+
+    def transformToDataFrame(self):
+        pass
+
+    def validateDataFrame(self):
+        pass
